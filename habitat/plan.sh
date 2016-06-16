359070de4ee8f6e79669299dad67d76ba350c0aa
Nolan Davidson <ndavidson@chef.io>
2016-06-16 10:56:00 -0400

    initial commit

---
+++ habitat/plan.sh
@@ -0,0 +1,33 @@
+pkg_origin=nolan
+pkg_name=rust-hello-world
+pkg_version=0.2.1
+pkg_maintainer="Nolan Davidson <ndavidson@chef.io>"
+pkg_license=()
+pkg_source=things
+pkg_shasum=stuff
+pkg_deps=(core/coreutils core/gcc-libs core/glibc)
+pkg_build_deps=(core/openssl core/rust core/cacerts core/gcc core/gcc-libs core/glibc)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_expose=(8080)
+pkg_service_run="./rust-hab-sample"
+
+do_download() {
+  return 0
+}
+
+do_unpack() {
+  cp -a ../ ${HAB_CACHE_SRC_PATH}
+}
+
+do_verify() {
+  return 0
+}
+do_build() {
+  env SSL_CERT_FILE=$(pkg_path_for cacerts)/ssl/cert.pem cargo build --release
+}
+
+do_install() {
+  cp ../target/release/rust-hab-sample ${pkg_prefix}
+}
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+#[macro_use] extern crate nickel;
+extern crate toml;
+extern crate redis;
+
+use nickel::Nickel;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use redis::Commands;
+
+fn main() {
+    let config_path = Path::new("config/config.toml");
+
+    let mut config_file = File::open(&config_path).unwrap();
+
+    let mut config_string = String::new();
+    config_file.read_to_string(&mut config_string).unwrap();
+
+    let config: toml::Value = config_string.parse().unwrap();
+    let port = config.lookup("app.port").unwrap();
+    let greeting = config.lookup("app.greeting").unwrap().clone();
+    let redis_host = config.lookup("app.redis_host").unwrap().clone();
+    let redis_port = config.lookup("app.redis_port").unwrap().clone();
+
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "/:name" => |req, _res| {
+            let name = req.param("name").unwrap().to_string();
+            let count = increment_count(redis_host.as_str().unwrap().to_string(), redis_port.as_str().unwrap().to_string(), name.clone());
+            format!("{} {}!  I have seen you {} times!", greeting.as_str().unwrap().to_string(), name, count.unwrap().to_string())
+        }
+    });
+
+    let server_string = format!("0.0.0.0:{}", port.as_str().unwrap()).to_string();
+    server.listen(&*server_string);
+}
+
+fn increment_count(host: String, port: String, name: String) -> redis::RedisResult<isize> {
+    let conn_string = format!("redis://{}:{}/", host, port);
+    let client = try!(redis::Client::open(&*conn_string));
+    let conn = try!(client.get_connection());
+
+    conn.incr(name, 1)
+}
