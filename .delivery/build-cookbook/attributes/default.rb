50345cfa0acabae36ce1735a0616395972a05bb7
Nolan Davidson <ndavidson@chef.io>
2016-07-28 21:35:42 -0400

    changing rust version attribute

--- .delivery/build-cookbook/attributes/default.rb
+++ .delivery/build-cookbook/attributes/default.rb
@@ -1,2 +1,2 @@
 default['habitat-build']['shellcheck-excludes'] = ['SC2034','SC2086', 'SC2046', 'SC2154']
-default['rustlang']['version'] = '1.11.0-nightly'
+default['rustlang']['version'] = 'nightly'
