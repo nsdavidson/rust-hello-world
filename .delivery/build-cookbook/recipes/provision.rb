b4c757eca67e035659d7852ab69d44efdeef6831
Nolan Davidson <ndavidson@chef.io>
2016-08-08 19:52:59 -0400

    fixes

--- .delivery/build-cookbook/recipes/provision.rb
+++ .delivery/build-cookbook/recipes/provision.rb
@@ -11,7 +11,8 @@ gh_token = data_bag_item('secrets', 'github')[:token]
 
 depot_url = node['habitat-build']['depot-url']
 stage = node['delivery']['change']['stage']
+ident = data_bag_item('rust-hello-world', node['applications']['rust-hello-world'])
 
 execute 'promote-hab-artifact' do
-  command "curl -I -X POST \"Authorization: Bearer #{gh_token}\" http://#{depot_url}/views/#{stage}/pkgs/"
+  command "curl -I -X POST -H \"Authorization: Bearer #{gh_token}\" #{depot_url}/views/#{stage}/pkgs/#{}"
 end
