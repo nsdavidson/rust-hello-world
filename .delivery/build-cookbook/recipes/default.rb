ffd494bc246cecc3897824c4c882cbba52e6e51c
Nolan Davidson <ndavidson@chef.io>
2016-08-09 14:56:46 -0400

    using data bag to pass build version

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'habitat-build::default'
-include_recipe 'rustlang'
+#include_recipe 'rustlang'
 package 'curl'
 
 tag('delivery-build-node')
--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,3 +4,17 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'habitat-build::publish'
+
+build_version = nil
+ruby_block 'load-build-output' do
+  block do
+    last_build_env = Hash[*::File.read(::File.join(hab_studio_path,
+                                                   'src/results/last_build.env')).split(/[=\n]/)]
+
+    build_version = [last_build_env['pkg_version'], last_build_env['pkg_release']].join('-')
+  end
+end
+
+db = data_bag_item('rust-hello-world', 'latest')
+db['latest'] = build_version
+db.save
