5472bcfbe5929a983422649972004b90c61650a0
Nolan Davidson <ndavidson@chef.io>
2016-08-09 09:16:54 -0400

    trying artifact_data

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,15 +4,3 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'habitat-build::publish'
-
-build_version = nil
-
-ruby_block 'save-build-version' do
-  block do
-    last_build_env = Hash[*::File.read(::File.join('results/last_build.env')).split(/[=\n]/)]
-
-    build_version = [last_build_env['pkg_version'], last_build_env['pkg_release']].join('-')
-  end
-end
-
-node.override['rust-hello-world']['build-version'] = build_version
