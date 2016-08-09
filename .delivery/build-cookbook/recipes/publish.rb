2fd7066e620af054832c8c7cb4df015b9661d1ee
Nolan Davidson <ndavidson@chef.io>
2016-08-09 15:18:06 -0400

    still trying

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -6,15 +6,11 @@
 include_recipe 'habitat-build::publish'
 
 build_version = nil
-ruby_block 'load-build-output' do
-  block do
-    last_build_env = Hash[*::File.read(::File.join(hab_studio_path,
-                                                   'src/results/last_build.env')).split(/[=\n]/)]
 
-    build_version = [last_build_env['pkg_version'], last_build_env['pkg_release']].join('-')
-  end
-end
+last_build_env = Hash[*::File.read(::File.join(hab_studio_path,
+                                                   'src/results/last_build.env')).split(/[=\n]/)]
+build_version = [last_build_env['pkg_version'], last_build_env['pkg_release']].join('-')
 
 db = data_bag_item('rust-hello-world', 'latest')
-db['latest'] = build_version
+db['build'] = build_version
 db.save
