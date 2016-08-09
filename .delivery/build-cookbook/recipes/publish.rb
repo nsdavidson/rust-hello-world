9595a00a1459202bb3760f741e825a945415d93b
Nolan Davidson <ndavidson@chef.io>
2016-08-08 22:37:02 -0400

    need that ident

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -9,8 +9,7 @@ build_version = nil
 
 ruby_block 'save-build-version' do
   block do
-    last_build_env = Hash[*::File.read(::File.join(hab_studio_path,
-                                                   'src/results/last_build.env')).split(/[=\n]/)]
+    last_build_env = Hash[*::File.read(::File.join('results/last_build.env')).split(/[=\n]/)]
 
     build_version = [last_build_env['pkg_version'], last_build_env['pkg_release']].join('-')
   end
