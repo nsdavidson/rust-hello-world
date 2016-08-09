ee59cd2a2a0a355519d13b656a2cdd6a4637aaba
Nolan Davidson <ndavidson@chef.io>
2016-08-09 16:15:31 -0400

    gh token problem

--- .delivery/build-cookbook/recipes/provision.rb
+++ .delivery/build-cookbook/recipes/provision.rb
@@ -1,18 +1,13 @@
-#
-# Cookbook Name:: build-cookbook
-# Recipe:: provision
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'habitat-build::provision'
 
 load_delivery_chef_config
 
-gh_token = data_bag_item('secrets', 'github')[:token]
+gh_token = data_bag_item('secrets', 'github')['token']
 latest = data_bag_item('rust-hello-world', 'latest')['build']
 depot_url = node['habitat-build']['depot-url']
 stage = node['delivery']['change']['stage']
 ident = data_bag_item('rust-hello-world', latest)['artifact']['pkg_ident']
 
 execute 'promote-hab-artifact' do
-  command "curl -I -X POST -H \"Authorization: Bearer #{gh_token}\" #{depot_url}/views/#{stage}/pkgs/#{}"
+  command "curl -I -X POST -H \"Authorization: Bearer #{gh_token}\" #{depot_url}/views/#{stage}/pkgs/#{ident}"
 end
