4ba8da62c7cf61ea968bff74bf07c45b37b374cd
Nolan Davidson <ndavidson@chef.io>
2016-08-08 20:40:14 -0400

    more fixes

--- .delivery/build-cookbook/recipes/provision.rb
+++ .delivery/build-cookbook/recipes/provision.rb
@@ -11,7 +11,7 @@ gh_token = data_bag_item('secrets', 'github')[:token]
 
 depot_url = node['habitat-build']['depot-url']
 stage = node['delivery']['change']['stage']
-ident = data_bag_item('rust-hello-world', node['applications']['rust-hello-world'])
+ident = data_bag_item('rust-hello-world', node['applications']['rust-hello-world']['build-version'])[:artifact][:pkg_ident]
 
 execute 'promote-hab-artifact' do
   command "curl -I -X POST -H \"Authorization: Bearer #{gh_token}\" #{depot_url}/views/#{stage}/pkgs/#{}"
--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,3 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'habitat-build::publish'
+
+ruby_block 'save-build-version' do
+  block do
+    last_build_env = Hash[*::File.read(::File.join(hab_studio_path,
+                                                   'src/results/last_build.env')).split(/[=\n]/)]
+
+    build_version = [last_build_env['pkg_version'], last_build_env['pkg_release']].join('-')
+  end
+end
+
+node['applications']['rust-hello-world']['build-version'] = build_version
